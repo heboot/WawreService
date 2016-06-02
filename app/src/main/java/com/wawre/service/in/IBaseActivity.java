72e07a01d37d1c2737b48b1742b33ca31c875102
Heboot <heboot@126.com>
2016-06-02 22:47:22 +0800

    update
    update

--- app/src/main/java/com/wawre/service/in/IBaseActivity.java
+++ app/src/main/java/com/wawre/service/in/IBaseActivity.java
@@ -11,17 +11,17 @@ public interface IBaseActivity {
     /**
      * 获取 Application 对象
      */
-    public abstract Application getApplication();
+    public   Application getApplication();
 
     /**
      * 判断是否有网络连接
      */
-    public abstract boolean validateInternet();
+    public   boolean validateInternet();
 
     /**
      * 显示Toast
      */
-    public abstract void showToast(String content,int duration);
+    public   void showToast(String content,int duration);
 
 
 
